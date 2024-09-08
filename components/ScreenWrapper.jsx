ba80c88bc7556c9904ed815dc3844954b6b380ae
AMAN RAJAN SINGH <madhusingh74055@gmail.com>
2024-09-08 16:02:46 +0530

    feat: authentication completed

--- components/ScreenWrapper.jsx
+++ components/ScreenWrapper.jsx
@@ -1,5 +1,6 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const ScreenWrapper = ({ children, bg }) => {
@@ -7,7 +8,11 @@ const ScreenWrapper = ({ children, bg }) => {
   const paddingTop = top > 0 ? top + 5 : 30;
 
   return (
-    <View style={{ flex: 1, paddingTop, backgroundColor: bg }}>{children}</View>
+    <View style={{ flex: 1, paddingTop, backgroundColor: bg }}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        {children}
+      </GestureHandlerRootView>
+    </View>
   );
 };
 
